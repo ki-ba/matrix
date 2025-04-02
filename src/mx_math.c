fb1a006b380069a7832051c067e9fc963018be49
Killian Barru <kbarru@z1r9p4.42lyon.fr>
2025-04-02 14:40:18 +0200

    store result of scalar product in double instead of int

--- src/mx_math.c
+++ src/mx_math.c
@@ -12,9 +12,9 @@
 
 #include "matrix.h"
 
-static int	calculate_product(t_matrix a, t_matrix b, size_t i, size_t j)
+static double	calculate_product(t_matrix a, t_matrix b, size_t i, size_t j)
 {
-	int		sum;
+	double	sum;
 	size_t	k;
 
 	k = 0;
@@ -35,7 +35,7 @@ void	mx_mult(t_matrix *p, t_matrix a, t_matrix b)
 
 	i = 0;
 	if (a.j != b.i)
-		ft_putstr_fd("Error : matrices aren't multipliable", 2);
+		ft_putstr_fd("Error : matrices aren't multipliable\n", 2);
 	else
 	{
 		create_null_matrix(p, a.i, b.j);
